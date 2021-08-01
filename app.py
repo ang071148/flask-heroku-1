528bef49519debc4af5c66747cb44118969f9c79
410-15pat <87839956+410-15pat@users.noreply.github.com>
2021-08-01 20:10:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,13 @@ data = [
 def hello():
     return "Hello Flask-Heroku"
 
-
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
+
+@app.route('/name')
+def name():
+    return"ปุญชรัสมิ์ จันทร์กลม เลขที่15 ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
