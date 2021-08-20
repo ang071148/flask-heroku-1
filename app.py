4cdf6dc52a3980706aa47090f8b6a3fec7b178e5
410-15pat <87839956+410-15pat@users.noreply.github.com>
2021-08-20 09:29:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return "<font color=red>ปุญชรัสมิ์ จันทร์กลม</font> <br> เลขที่15 ม.4/10"
+    return "<font color=purple>ปุญชรัสมิ์ จันทร์กลม</font> <br> เลขที่15 ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
